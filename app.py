3a0d59bf44d1066d763761573b3efa9a3f70058f
Mazin Hassan <mazinmhassan@gmail.com>
2024-12-18 12:32:05 -0500

    push changes

--- app.py
+++ app.py
@@ -2,5 +2,11 @@ import pandas as pd
 import scipy.stats
 import streamlit as st
 import time
+import plotly_express as px
 
+#Name of App showing used car lot inventory
 st.header('Used Car Info')
+
+#read CSV data
+
+
